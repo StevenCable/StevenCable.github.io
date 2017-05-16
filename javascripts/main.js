41784972c358efee1814395b6586cd4143d4ee90
Steven Cable <steven.cable12@gmail.com>
2017-05-15 19:55:52 -1000

    updated per Lori comments

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,127 @@
-console.log('This would be the main JS file.');
+# Steven Cable, E.I.T.
+
+1. Steven.Cable12@gmail.com
+1. (360) 790-1927
+1. [Github](https://github.com/stevencable)
+1. [LinkedIn](https://www.linkedin.com/in/steven-cable-385a3050/)
+
+## Objective
+Seeking to resolve my promise to make the world a better place, one commit at a time.
+
+## Development Experience
+
+### StratosPeer
+#### [StratosPeer](https://github.com/StevenCable/stratospeer)
+##### May 2017
+- StratosPeer will aggregate all your files across supported cloud services providing quick and easy access, all right from our desktop application. Application implements React Redux,CSS3 with SASS, Sequelize, Express, NodeJS, Redis, and PostreSQL wrapped in Electron from Atom.io.  
+
+**My Contributions: ** Implementing Github API with OAuth2, Server side RESTful routing using Express and Sequelize, Data aggregation, parsing, and formatting for front-end React, Fuzzy Filter Search functionality, and Electron packaging. Current deployed version supports `Github` and `Box.com` services with more on the way!
+
+### Express Gallery
+#### [Express Gallery](https://github.com/StevenCable/express-gallery)
+##### March 2017
+- A multi-user photo gallery using Express, Sequelize, HTML5 with Handlebars, and CSS3 with SASS C.R.U.D. web application. All data stored on PostgreSQL. 
+- User profiles maintain personal photos or access to entire gallery of all users
+- Authorization at login through Passport and bcrypt with salt
+- Implemented Admin role with elevated priviledges
+
+### KanBan Board with React
+#### [React KanBan](https://github.com/StevenCable/React-Kanban)
+##### March 2017
+- A To-Do taskboard using React Redux with RESTful API routing via Express.
+- Runs on PostgreSQL for a user database implemented through Sequelize.
+
+### Alexa Theme-r
+#### [Alexa Themer](https://github.com/edsperanto/att--alexa-themer)
+##### March 2017 - AT&T 48hr Hack-a-Thon
+- Alexa Simulator with the ability to detect specific users by Phone ID and play a specific (user set) theme song unique to each user. Have your own theme song whenever you come home and have Simlexa greet you! 
+
+** My Contributions: ** Generate project backbone structure for frontend/backend, Implement: expressJS, initial server structure using Sequelize and Express, and Authentication checks using Passport and Salt.
+
+### Adware-WAAVE
+#### [Adware Waves](https://github.com/DEbinger/adware-waves )
+##### January 2017 - Global Game Jam Hack-a-Thon
+- A game that loads pop ups in intervals/set timeouts to your workstation. Once the ad has fully resolved it will crash your work station and plug it's product for good measure! 
+
+
+### WellThisIsFrustrating
+#### [WellThisIsFrustrating](https://github.com/DEbinger/WellThisIsFrustrating)
+##### January 2017 - SSH Hack-a-Thon
+- Google Chrome Extension that does it's very best to make your experience on the internet it's very worst. Placed 1st in Chrome Extension Category at S.S.H.
+
+## Professional Experience
+
+### Project Manager (General Contracting for Construction)
+#### Nan, Inc.
+##### Honolulu, Hawaii 2013 – Present  
+- Project Manager - Pacific Air Force Headquarters Renovation Phase 6, Hickam AFB, HI, 2017 ~ $6M (14 months)
+- Project Manager - Facility Modifications for P861, Kaneohe Marine Corps Base, HI, 2017 ~ $42M (38 months)
+  -Currently on schedule and 8% under budget.
+- Asst. Quality Control Manager - Pacific Air Force Headquarters Renovation Phase 5, Hickam AFB, HI, 2016 ~ $45M (28 months)
+  - Currently ~1 Month ahead of schedule and under budget.
+- Quality Control Manager - Pacific Air Force Headquarters Renovation Phase 4, Hickam AFB, HI, 2015 ~ $18M (14 months)
+  - Completed on time with 11% Profit.
+  - Renovation project of the year West Region.
+
+## Education
+### DevLeague
+#### Fast paced, and immersive full-stack Javascript web development mentorship program with over 800 hours of instruction time and countless more crunch time. Topics include work flows and version management strategies, server side development and integration with both SQL and noSQL databases, front end frameworks and styling. 
+##### September 2016 - May 2017
+
+### Bachelor of Science in Mechanical Engineering
+#### **Minor in Business Administration **
+##### Saint Martin's University 2006 – 2011 
+##### Licensed Engineer In Training (E.I.T.) - 2011
+
+  - American Society of Mechanical Engineers Secretary 2010-2011
+  - Men's Golf team 2008-2011, Team Captain 2010-2011
+  - Dean's List 2008-2011 
+
+## Technical Skills
+
+### Languages
+- JavaScript
+- HTML5
+- SQL
+- noSQL
+- CSS3
+
+### Web Skills
+- Responsive/Mobile First Web Development
+- API Integration (GoogleDrive, Github, Box.com, Instagram, Reddit)
+- Relational - and Non-Relational Database Modeling 
+- OAuth2 integration
+- User Authentication
+- Google Chrome Extensions
+- Swagger.io
+
+### Frontend Proficiencies
+- JavaScript
+- HTML5
+- CSS3
+- ES6
+- React
+- Redux
+- Handlebars
+- Electron
+- SASS
+- Angular
+
+### Server-Side Proficiencies
+- NodeJS
+- Express
+- Sequelize
+- OAuth2
+- Passport
+- bcrypt
+
+### Database Proficiencies
+- PostgreSQL 
+- Redis
+
+### Workflow/Testing Utilities
+- Git
+- Gulp
+- Mocha
+- Chai
+- SuperTest
